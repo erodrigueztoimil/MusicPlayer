4ac8b9871124a03a38ad9d3c02e9788899c29506
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-03-17 21:32:14 -0400

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Ernesto Rodriguez on 3/17/20.
+//  Copyright © 2020 Ernesto Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
